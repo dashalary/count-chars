fe62fc8479ec486a1aabe9a9830c2da7a7677aab
Dasha Lary <dasha4988@gmail.com>
2021-06-18 17:27:23 -0400

    finished build and run

--- main.c
+++ main.c
@@ -5,5 +5,19 @@ int stringLength(const char *string);
 
 int main()
 {
+    printf("%d \n", stringLength("stringLength testing"));
+    printf("%d \n", stringLength(""));
+    printf("%d \n", stringLength("another test"));
+
     return 0;
+}
+
+int stringLength(const char *string)
+{
+    const char *lastAddress = string;
+
+    while (*lastAddress)
+        ++lastAddress;
+
+    return lastAddress - string;
 }
